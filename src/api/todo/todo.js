d8a3cae2e98b9e5f67628fc6f93f560b1b57d650
VictorHSP <victor.hugo.s@hotmail.com>
2018-11-18 02:08:05 -0200

    Configurando schema no mongodb e seus metodos para CRUD

---
+++ src/api/todo/todo.js
@@ -0,0 +1,14 @@
+const restful = require('node-restful')
+const mongoose = restful.mongoose
+
+/**
+ * Criando schema no mongodb
+ *
+ * */
+const todoSchema = new mongoose.Schema({
+    description: { type: String, required: true },
+    done: { type: Boolean, required: true, default: false },
+    createdAt: { type: Date, default: Date.now }
+})
+
+module.exports= restful.model('Todo', todoSchema)
